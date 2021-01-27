7ad25fedcfab3f231d61dacef18ab965d320e55e
Adithya931 <76829988+Adithya931@users.noreply.github.com>
2021-01-27 21:19:15 +0530

    Add files via upload

---
+++ python1p.py
@@ -0,0 +1,5 @@
+color=input(" Enter a set of colors(space seperated):")
+coior2=input("Enter a set of colors(space seperated):")
+c1=set(color1.split())
+c2=set(color2.split())
+print("The difference between",c1,"and",c2,"is",c1.difference(c2))
